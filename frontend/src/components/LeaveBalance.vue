ccf4fcd2ee5505c0eea54817905a8df58a14dda3
Asmita Hase <asmitahase@gmail.com>
2025-05-20 17:20:53 +0530

    feat: show team attendance requests under team requests
    (cherry picked from commit e25b746ec15bd557cb3e68f2162aa968afbb5fa9)

--- frontend/src/components/LeaveBalance.vue
+++ frontend/src/components/LeaveBalance.vue
@@ -45,7 +45,7 @@
 
 <script setup>
 import SemicircleChart from "@/components/SemicircleChart.vue"
-
+import { inject } from "vue"
 import { leaveBalance } from "@/data/leaves"
 import { inject } from "vue"
 
